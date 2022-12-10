b4b9e845270e9d3be7e293c2a08c76e080a52bd4
Tuuux <tuxa@rtnp.org>
2022-12-10 15:14:05 +0100

    Better Normal mode color

--- lua/lualine/themes/darcula.lua
+++ lua/lualine/themes/darcula.lua
@@ -3,8 +3,8 @@ local colors = require('darcula.colors')
 local darcula = {}
 
 darcula.normal = {
-	a = {fg = colors.menuFg, bg = colors.ANSIDarkGray, gui = 'bold'},
-	b = {fg = colors.menuFg, bg = colors.ANSIDarkGray, gui = 'bold'},
+	a = {fg = colors.statusLineFg, bg = colors.statusLine, gui = 'bold'},
+	b = {fg = colors.statusLineFg, bg = colors.statusLine, gui = 'bold'},
 	c = {fg = colors.menuFg, bg = colors.menu},
 }
 
