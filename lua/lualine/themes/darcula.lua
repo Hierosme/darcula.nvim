e5b199329fc24d834025b28ea30414e08c84ab64
Tuuux <tuxa@rtnp.org>
2022-12-10 14:55:04 +0100

    Better color for lualine

--- lua/lualine/themes/darcula.lua
+++ lua/lualine/themes/darcula.lua
@@ -4,34 +4,33 @@ local darcula = {}
 
 darcula.normal = {
 	a = {fg = colors.bg, bg = colors.ANSIWhite, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-	c = {fg = colors.fg, bg = colors.selection},
+	b = {fg = colors.title, bg = colors.menu},
+	c = {fg = colors.fg, bg = colors.menu},
 }
 
 darcula.insert = {
-	a = {fg = colors.bg, bg = colors.ANSIBrightGreen, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = {fg = colors.menu, bg = colors.ANSIBrightGreen, gui = 'bold'},
+	b = {fg = colors.title, bg = colors.menu},
 }
 
 darcula.visual = {
-	a = {fg = colors.bg, bg = colors.ANSIBrightBlue, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = {fg = colors.menu, bg = colors.ANSIBrightBlue, gui = 'bold'},
+	b = {fg = colors.menuFg, bg = colors.menu},
 }
 
 darcula.replace = {
-	a = {fg = colors.bg, bg = colors.ANSIBrightRed, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	a = {fg = colors.menu, bg = colors.ANSIBrightRed, gui = 'bold'},
+	b = {fg = colors.menuFg, bg = colors.menu},
 }
 
 darcula.command = {
 	a = {fg = colors.bg, bg = colors.ANSIBrightYellow, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+	b = {fg = colors.menuFg, bg = colors.menu},
 }
 
 darcula.inactive = {
-  a = {fg = colors.disabled, bg = colors.bg, gui = 'bold'},
-  b = {fg = colors.disabled, bg = colors.bg},
-  c = {fg = colors.disabled, bg = colors.selection}
+  a = {fg = colors.disabled, bg = colors.menu, gui = 'bold'},
+  b = {fg = colors.disabled, bg = colors.menu},
+  c = {fg = colors.disabled, bg = colors.menu}
 }
-
 return darcula
