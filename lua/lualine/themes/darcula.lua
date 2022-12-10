e54d5f51666ee6fc5ab8adf9d2691a2600d8994c
Tuuux <tuxa@rtnp.org>
2022-12-10 11:45:59 +0100

    Try colors for lualine

--- lua/lualine/themes/darcula.lua
+++ lua/lualine/themes/darcula.lua
@@ -3,28 +3,28 @@ local colors = require('darcula.colors')
 local darcula = {}
 
 darcula.normal = {
-	a = {fg = colors.bg, bg = colors.accent, gui = 'bold'},
+	a = {fg = colors.bg, bg = colors.ANSIWhite, gui = 'bold'},
 	b = {fg = colors.title, bg = colors.active},
 	c = {fg = colors.fg, bg = colors.selection},
 }
 
 darcula.insert = {
-	a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
+	a = {fg = colors.bg, bg = colors.ANSIBrightGreen, gui = 'bold'},
 	b = {fg = colors.title, bg = colors.active},
 }
 
 darcula.visual = {
-	a = {fg = colors.bg, bg = colors.purple, gui = 'bold'},
+	a = {fg = colors.bg, bg = colors.ANSIBrightBlue, gui = 'bold'},
 	b = {fg = colors.title, bg = colors.active},
 }
 
 darcula.replace = {
-	a = {fg = colors.bg, bg = colors.red, gui = 'bold'},
+	a = {fg = colors.bg, bg = colors.ANSIBrightRed, gui = 'bold'},
 	b = {fg = colors.title, bg = colors.active},
 }
 
 darcula.command = {
-	a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'},
+	a = {fg = colors.bg, bg = colors.ANSIBrightYellow, gui = 'bold'},
 	b = {fg = colors.title, bg = colors.active},
 }
 
